872b3a335b7cc39018b680985cc035fa95184be3
Zendy <nguyenduc.051194@gmail.com>
2021-10-28 22:55:52 +0700

    Setup project

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// Colors that we use in our app
+const Color kPrimaryColor = Color(0xFF0C9869);
+const Color kTextColor = Color(0xFF3C4046);
+const Color kBackgroundColor = Color(0xFFF9F8FD);
+
+const double kDefaultPadding = 20.0;
